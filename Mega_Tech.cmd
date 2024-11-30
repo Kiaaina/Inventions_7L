e726c4f6f92f7c1173636d57a7e7fd0a5543cc8e
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-11-30 11:10:53 -1000

    Add files via upload

---
+++ Mega_Tech.cmd
@@ -0,0 +1,12 @@
+cd /
+title "Mega_Link7L"
+mode 30,2
+echo off
+color 74
+cls
+
+:top
+start https://mega.nz/folder/JCkknbTZ#gaXn8OCBBH1_F9MoRX0Qwg
+echo Mega_By_Kia7L...
+(timeout /t 5) > nul
+exit
